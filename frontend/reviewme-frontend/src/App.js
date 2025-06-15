dcc1789b6e1496aac06314896905bc0e172441e7
omerawwad <aomerashraf22@gmail.com>
2025-06-15 15:25:49 +0300

    added frontend intial files

---
+++ frontend/reviewme-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  // const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  // console.log("Backend URL:", BACKEND_URL);
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;

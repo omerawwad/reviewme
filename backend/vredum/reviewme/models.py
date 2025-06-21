195954e23832473a28ed112aa94bf414518bad2c
omerawwad <aomerashraf22@gmail.com>
2025-06-21 22:40:16 +0300

    converted login with username to email

--- backend/vredum/reviewme/models.py
+++ backend/vredum/reviewme/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
     def __str__(self):
         return self.username
     
